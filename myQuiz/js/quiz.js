28e7366b55ac43e7763404ffc3f983ed6184403d
Manuel Lema <manuel.b.lema@gmail.com>
2016-10-22 13:33:54 -0400

    Finished project
    only on bug left to fix - wrong answers won’t highlight :(

--- myQuiz/js/quiz.js
+++ myQuiz/js/quiz.js
@@ -21,6 +21,7 @@
 			if( questionState != 'answered' ){
 				$scope.myQuestions[qIndex].selectAnswer = aIndex;
 				var correctAnswer = $scope.myQuestions[qIndex].correct;
+				$scope.myQuestions[qIndex].correctAnswer = correctAnswer;
 
 				if( aIndex === correctAnswer ){
 					$scope.myQuestions[qIndex].correctness = 'correct';
@@ -30,10 +31,32 @@
 				}
 				$scope.myQuestions[qIndex].questionState = 'answered';
 			}
-
+			$scope.percentage = (($scope.score / $scope.totalQuestions)*100).toFixed(1);
 		}
 
+		$scope.isSelected = function(qIndex,aIndex){
+			return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+		}
+		$scope.isCorrect = function(qIndex,aIndex){
+			return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+		}
 
+		$scope.selectContinue = function(){
+			return $scope.activeQuestion += 1;
+		}
+
+		$scope.createShareLinks = function (percentage) {
+			var url = 'http://www.google.com';
+
+			var emailLink = '<a class="btn email" href="mailto:?subject=try to beat my quiz score!&amp;body=I scored a '+percentage+'% on this quiz. Try to beat my score at '+url+'">Email a friend</a>';
+
+			var twitterLink = '<a class="btn twitter" target="_blank" href="http://twitter.com/share?text=I scored '+percentage+' here. Try to beat me at &url='+url+'&hashtags=SaturnQuiz">Tweet your score</a>';
+
+			var newMarkup = emailLink + twitterLink;
+
+			return $sce.trustAsHtml(newMarkup);
+
+		}
 
 	}]);
 
